faf639a2aa8d2abac048700492dfd23971db2d31
Eduardo Gonzalez Melgoza <eduardogonzalezmelgoza@10-20-56-147.0200.fed.it.iosda.org>
2023-12-06 16:59:13 +0100

    Initial commit

---
+++ OpAwesome/OpAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpAwesomeApp.swift
+//  OpAwesome
+//
+//  Created by Eduardo Gonzalez Melgoza on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct OpAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
